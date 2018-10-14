dbd7816dfa6a96065a4d49ab713548a1b0811afc
Burcu Söylemez <burcu.soylemez@bil.omu.edu.tr>
2018-10-14 16:39:43 +0300

    Generate random number and guess it

--- guessinggame.py
+++ guessinggame.py
@@ -1,13 +1,13 @@
 from random import randint #For generate random number
 
-randnum = randint(1, 200)
+randNum = randint(1, 200)
 print("Let's guess a number between 1 and 200!")
 while True:
-    guessnum = int(input("Tell me your guess:\t"))
-    if guessnum > randnum:
+    guessNum = int(input("Tell me your guess:\t"))
+    if guessNum > randNum:
         print("Enter a smaller number")
-    elif randnum > guessnum:
+    elif randNum > guessNum:
         print("Enter a bigger number")
     else:
-        print("You found it! The number was {0}".format(randnum))
+        print("You found it! The number was {0}".format(randNum))
         break
